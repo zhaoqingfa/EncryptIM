d74c6586642159e80e6b2f29bfe4f8cdb9b4617b
zhaoqingfa <v_zhaoqingfa@baidu.com>
2018-12-20 21:03:21 +0800

    update xscroll

---
+++ app/src/main/java/com/encrypt/im/common/widgets/xscroll/XHeaderView.java
@@ -0,0 +1,175 @@
+package com.encrypt.im.common.widgets.xscroll;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.encrypt.im.R;
+
+/**
+ * Created by v_zhaoqingfa on 2018/12/20.
+ */
+
+public class XHeaderView extends LinearLayout {
+    public final static int STATE_NORMAL = 0;
+    public final static int STATE_READY = 1;
+    public final static int STATE_REFRESHING = 2;
+
+    private final int ROTATE_ANIM_DURATION = 180;
+
+    private LinearLayout mLayout;
+
+    private RelativeLayout mHeaderContent;
+
+    private ImageView mArrowImageView;
+
+    private ProgressBar mProgressBar;
+
+    private TextView mHintTextView;
+
+    private TextView mHintTextTime;
+
+    private int mState = STATE_NORMAL;
+
+    private Animation mRotateUpAnim;
+    private Animation mRotateDownAnim;
+
+    private boolean mIsFirst;
+
+    public XHeaderView(Context context) {
+        super(context);
+        init(context);
+    }
+
+    public XHeaderView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init(context);
+    }
+
+    public XHeaderView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context);
+    }
+
+    private void init(Context context) {
+        // Initial set header view height 0
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, 0);
+        mLayout = (LinearLayout) LayoutInflater.from(context).inflate(R.layout.view_xheader, null);
+        addView(mLayout, lp);
+        setGravity(Gravity.BOTTOM);
+
+        mHeaderContent = (RelativeLayout) findViewById(R.id.mrl_header_content);
+        mArrowImageView = (ImageView) findViewById(R.id.miv_header_arrow);
+        mHintTextView = (TextView) findViewById(R.id.mtv_header_hint_text);
+        mHintTextTime = (TextView) findViewById(R.id.mtv_header_hint_time);
+        mProgressBar = (ProgressBar) findViewById(R.id.mpb_header_progressbar);
+
+        mRotateUpAnim = new RotateAnimation(0.0f, -180.0f, Animation.RELATIVE_TO_SELF, 0.5f,
+                Animation.RELATIVE_TO_SELF, 0.5f);
+        mRotateUpAnim.setDuration(ROTATE_ANIM_DURATION);
+        mRotateUpAnim.setFillAfter(true);
+
+        mRotateDownAnim = new RotateAnimation(-180.0f, 0.0f, Animation.RELATIVE_TO_SELF, 0.5f,
+                Animation.RELATIVE_TO_SELF, 0.5f);
+        mRotateDownAnim.setDuration(ROTATE_ANIM_DURATION);
+        mRotateDownAnim.setFillAfter(true);
+    }
+
+    public void setState(int state) {
+        if (state == mState && mIsFirst) {
+            mIsFirst = true;
+            return;
+        }
+
+        if (state == STATE_REFRESHING) {
+            // show progress
+            mArrowImageView.clearAnimation();
+            mArrowImageView.setVisibility(View.INVISIBLE);
+            mProgressBar.setVisibility(View.VISIBLE);
+        } else {
+            // show arrow image
+            mArrowImageView.setVisibility(View.VISIBLE);
+            mProgressBar.setVisibility(View.INVISIBLE);
+        }
+
+        switch (state) {
+            case STATE_NORMAL:
+                if (mState == STATE_READY) {
+                    mArrowImageView.startAnimation(mRotateDownAnim);
+                }
+
+                if (mState == STATE_REFRESHING) {
+                    mArrowImageView.clearAnimation();
+                }
+
+                mHintTextView.setText(R.string.x_header_hint_refresh_normal);
+                break;
+
+            case STATE_READY:
+                if (mState != STATE_READY) {
+                    mArrowImageView.clearAnimation();
+                    mArrowImageView.startAnimation(mRotateUpAnim);
+                    mHintTextView.setText(R.string.x_header_hint_refresh_ready);
+                }
+                break;
+
+            case STATE_REFRESHING:
+                mHintTextView.setText(R.string.x_header_hint_refresh_loading);
+                break;
+
+            default:
+                break;
+        }
+
+        mState = state;
+    }
+
+    /**
+     * Set the header view visible height.
+     *
+     * @param height
+     */
+    public void setVisibleHeight(int height) {
+        if (height < 0) height = 0;
+        LinearLayout.LayoutParams lp = (LinearLayout.LayoutParams) mLayout.getLayoutParams();
+        lp.height = height;
+        mLayout.setLayoutParams(lp);
+    }
+
+    /**
+     * Get the header view visible height.
+     *
+     * @return
+     */
+    public int getVisibleHeight() {
+        return mLayout.getLayoutParams().height;
+    }
+
+    /**
+     * Get the header view visible height.
+     *
+     * @return
+     */
+    public int getHeaderContentHeight() {
+        return mHeaderContent.getHeight();
+    }
+
+    /**
+     * Set last refresh time
+     *
+     * @param time
+     */
+    public void setRefreshTime(String time) {
+        mHintTextTime.setText(time);
+    }
+}
--- app/src/main/java/com/encrypt/im/common/widgets/xscroll/XScrollView.java
+++ app/src/main/java/com/encrypt/im/common/widgets/xscroll/XScrollView.java
@@ -1,14 +1,45 @@
 package com.encrypt.im.common.widgets.xscroll;
 
+import android.annotation.TargetApi;
 import android.content.Context;
+import android.os.Build;
+import android.support.v7.widget.RecyclerView;
 import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewTreeObserver;
+import android.widget.AbsListView;
+import android.widget.LinearLayout;
 import android.widget.ScrollView;
+
+import com.encrypt.im.R;
 
 /**
  * Created by zqf on 2018/12/19.
  */
 
-public class XScrollView extends ScrollView {
+public class XScrollView extends ScrollView implements AbsListView.OnScrollListener{
+
+    // support iOS like pull
+    private final static float OFFSET_RADIO = 1.8f;
+
+    private LinearLayout mHeaderLayout;
+    private LinearLayout mContentLayout;
+    private LinearLayout mFooterLayout;
+
+    private XHeaderView mHeaderView;
+    private XFooterView mFooterView;
+
+    private int mHeaderContentHeight;
+
+    private boolean mIsShowHeaderView;
+    private boolean mIsShowFooterView;
+    private boolean mIsEnablePullRefresh;
+    private boolean mIsEnablePullLoad;
+
+    private float mLastY = -1;
+
     public XScrollView(Context context) {
         super(context);
         init(context);
@@ -25,6 +56,210 @@ public class XScrollView extends ScrollView {
     }
 
     private void init(Context context) {
+        LayoutInflater.from(context).inflate(R.layout.view_xscroll, this);
+        mContentLayout = (LinearLayout) findViewById(R.id.mll_content_layout);
+        mHeaderLayout = (LinearLayout) findViewById(R.id.mll_header_layout);
+        mFooterLayout = (LinearLayout) findViewById(R.id.mll_footer_layout);
+
+//        this.setOnScrollChangeListener(this);
+
+        // init header view
+        mHeaderView = new XHeaderView(context);
+        mHeaderLayout.addView(mHeaderView);
+
+        // init footer view
+        mFooterView = new XFooterView(context);
+        mFooterLayout.addView(mFooterView);
+
+        // init header height
+        ViewTreeObserver observer = mHeaderView.getViewTreeObserver();
+        if (null != observer) {
+            observer.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+                @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+                @SuppressWarnings("deprecation")
+                @Override
+                public void onGlobalLayout() {
+                    mHeaderContentHeight = mHeaderView.getHeaderContentHeight();
+                    ViewTreeObserver observer = getViewTreeObserver();
+                    if (null != observer) {
+                        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN) {
+                            observer.removeGlobalOnLayoutListener(this);
+                        } else {
+                            observer.removeOnGlobalLayoutListener(this);
+                        }
+                    }
+                }
+            });
+        }
+    }
+
+    /**
+     * Set the content ViewGroup for XScrollView.
+     *
+     * @param content
+     */
+    public void setContentView(View content) {
+        if (mContentLayout.getChildCount() > 0) {
+            mContentLayout.removeAllViews();
+        }
+        mContentLayout.addView(content);
+    }
+
+    /**
+     * Enable or disable pull down refresh feature.
+     *
+     * @param enable
+     */
+    public void setPullRefreshEnable(boolean enable) {
+        setPullRefreshEnable(enable, true);
+    }
+
+    /**
+     * Enable or disable pull down refresh feature.
+     * @param enable
+     * @param isShowHeader
+     */
+    public void setPullRefreshEnable(boolean enable, boolean isShowHeader) {
+        mIsEnablePullRefresh = enable;
+        mIsShowHeaderView = isShowHeader;
+        mHeaderView.setVisibility(enable && isShowHeader ? View.VISIBLE : View.INVISIBLE);
+    }
+
+//    /**
+//     * Enable or disable pull up load more feature.
+//     *
+//     * @param enable
+//     */
+//    public void setPullLoadEnable(boolean enable) {
+//        mIsEnablePullRefresh = enable;
+//
+//        if (!mEnablePullLoad) {
+//            mFooterView.setBottomMargin(0);
+//            mFooterView.hide();
+//            mFooterView.setPadding(0, 0, 0, mFooterView.getHeight() * (-1));
+//            mFooterView.setOnClickListener(null);
+//
+//        } else {
+//            mPullLoading = false;
+//            mFooterView.setPadding(0, 0, 0, 0);
+//            mFooterView.show();
+//            mFooterView.setState(XFooterView.STATE_NORMAL);
+//            // both "pull up" and "click" will invoke load more.
+//            mFooterView.setOnClickListener(new OnClickListener() {
+//                @Override
+//                public void onClick(View v) {
+//                    startLoadMore();
+//                }
+//            });
+//        }
+//    }
+
+    /**
+     * Stop refresh, reset header view.
+     */
+    public void stopRefresh() {
+//        if (mPullRefreshing) {
+//            mPullRefreshing = false;
+//            resetHeaderHeight();
+//        }
+    }
+
+    /**
+     * Stop load more, reset footer view.
+     */
+    public void stopLoadMore() {
+//        if (mPullLoading) {
+//            mPullLoading = false;
+//            mFooterView.setState(XFooterView.STATE_NORMAL);
+//        }
+    }
+    /**
+     * Set last refresh time
+     *
+     * @param time
+     */
+    public void setRefreshTime(String time) {
+        mHeaderView.setRefreshTime(time);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        if (mLastY == -1) {
+            mLastY = ev.getRawY();
+        }
+
+        switch (ev.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                mLastY = ev.getRawY();
+                break;
+
+            case MotionEvent.ACTION_MOVE:
+                final float deltaY = ev.getRawY() - mLastY;
+                mLastY = ev.getRawY();
+
+                if (isTop() && (mHeaderView.getVisibleHeight() > 0 || deltaY > 0)) {
+                    // the first item is showing, header has shown or pull down.
+                    updateHeaderHeight(deltaY / OFFSET_RADIO);
+//                    invokeOnScrolling();
+
+//                } else if (isBottom() && (mFooterView.getBottomMargin() > 0 || deltaY < 0)) {
+//                    // last item, already pulled up or want to pull up.
+//                    updateFooterHeight(-deltaY / OFFSET_RADIO);
+
+                }
+                break;
+
+            default:
+                // reset
+                mLastY = -1;
+
+//                resetHeaderOrBottom();
+                break;
+        }
+
+        return super.onTouchEvent(ev);
+    }
+
+    private boolean isTop() {
+        return getScrollY() <= 0 || mHeaderView.getVisibleHeight() > mHeaderContentHeight
+                || mContentLayout.getTop() > 0;
+    }
+
+    private void updateHeaderHeight(float delta) {
+        mHeaderView.setVisibleHeight((int) delta + mHeaderView.getVisibleHeight());
+
+//        if (mEnablePullRefresh && !mPullRefreshing) {
+//            // update the arrow image unrefreshing
+//            if (mHeader.getVisibleHeight() > mHeaderHeight) {
+//                mHeader.setState(XHeaderView.STATE_READY);
+//            } else {
+//                mHeader.setState(XHeaderView.STATE_NORMAL);
+//            }
+//        }
+
+        // scroll to top each time
+        post(new Runnable() {
+            @Override
+            public void run() {
+                XScrollView.this.fullScroll(ScrollView.FOCUS_UP);
+            }
+        });
+    }
+
+//    private boolean isBottom() {
+//        return Math.abs(getScrollY() + getHeight() - computeVerticalScrollRange()) <= 5 ||
+//                (getScrollY() > 0 && null != mFooterView && mFooterView.getBottomMargin() > 0);
+//    }
+
+    @Override
+    public void onScrollStateChanged(AbsListView view, int scrollState) {
 
     }
+
+    @Override
+    public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+
+    }
+
+
 }
